6e899d7749386e58a2fbe047147c4120e7f27ac0
Marc LaFleur <marc@massivescale.com>
2016-07-12 12:59:29 -0400

    Added sample recording

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 const rmv = require('./lib/rmv.js');
-
 const express = require('express');
 
 var app = express();
 var port = process.env.port || 3000
 
+//var branchData;
 
 app.get('/', function (req, res) {
-    rmv.getAll(function (result) {
-        res.type('json');
-        res.send(JSON.stringify(result, null, 4));
-
+    rmv.getBranchData(function (branchData) {
+        rmv.getRmvData(function (rmvData) {
+            rmv.mergeData(branchData, rmvData, function (result) {
+                res.type('json');
+                res.send(JSON.stringify(result, null, 4));
+            });
+        });
     });
 });
 
 app.get('/branches', function (req, res) {
-    rmv.getBranchData(function (result) {
-        res.type('json');
-        res.send(JSON.stringify(result, null, 4));
-    });
+    res.type('json');
+    res.send(JSON.stringify(branchData, null, 4));
 });
 
 app.get('/rmv', function (req, res) {
@@ -35,42 +36,6 @@ app.get('/record', function (req, res) {
     });
 });
 
-/*
-app.get('/branches/', function (req, res) {
-    updateBranchStatus(function (branchList) {
-        res.type('json');
-        res.send(JSON.stringify(branchList, null, 4));
-    });
-});
-*/
-
 var server = app.listen(port, function () {
-
-    //getBranchList();
+//    rmv.getBranchData(function (data) { branchData = data });
 });
-
-/*
-
-function updateBranchStatus(callback) {
-    getRmvData(function (data) {
-        for (var branchId in data.branches.branch) {
-            if (data.branches.branch.hasOwnProperty(branchId)) {
-                var branch = data.branches.branch[branchId];
-                branchList[branch.town[0]].Licensing = branch.licensing[0];
-                branchList[branch.town[0]].Registration = branch.registration[0];
-            }
-        }
-        callback(branchList);
-    });
-}
-
-
-function getRmvData(callback) {
-    request('http://www.massdot.state.ma.us/feeds/qmaticxml/qmaticXML.aspx', function (err, response, body) {
-        parseString(body, function (err, result) {
-            if (callback) callback(result);
-        });
-    });
-}
-
-*/
